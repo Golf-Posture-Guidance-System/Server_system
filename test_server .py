d5af2dca2fdf5afc3fbe3d9382e31c28b908f97f
조재원 <m970314@naver.com>
2021-06-24 00:03:11 +0900

    final

--- test_server .py
+++ test_server .py
@@ -5,22 +5,20 @@ from module.classify import *
 from module.draw import *
 from module.assess import *
 
-
-INF = 10000
-error = 0
-
-
-
-filename = 'badpose'
-vidname = filename + '.mp4'
-pathname = 'examples/media/' + vidname
+global score
+global feedback_list
+global error
+# date = os.system('date')
+filepath = '../openpose/examples/media/'
+filename = 'TestGolf'
+vidname = filepath + filename + '.mp4'
 # 아래 주석풀면 gpu 과부하걸리니 최초 실행시만
-#path = os.system('../openpose/build/examples/openpose/openpose.bin --video ' + pathname +  ' --write_json output/ --display 0 --render_pose 0')
-size, frame = get_frame(vidname,pathname)
-posepoints = get_keypoints(filename, size)
-pose_idx = pose_classifier(posepoints)  # 포즈 분류하기
-pose_img = cut_vid(frame, pose_idx)  # mat이미지 반환받기
-draw_image(posepoints,pose_img, pose_idx)  # 골격 그리기
-cut_img(posepoints, pose_img, pose_idx, 0)  # 서버에 전송할 7가지 이미지 자르기(포즈 자세히 부분에 사용자에게 보여줄거)
-score_list = assess_pose(posepoints,pose_idx) #포즈 평가하기
-#worst의 경우.  [1 : 치킨윙 발생 , 2:바디스웨이 발생 , 3:탑스윙 구현 x]  # 가장 심각한 실수가 어떤것지 찾기위해
+path = os.system(
+    '../openpose/build/examples/openpose/openpose.bin --video ' '../openpose/examples/media/TestGolf.mp4 --write_json output/ --display 0 --render_pose 0')
+size, frame = get_frame(vidname)
+posepoints, error = get_keypoints(filename, size)
+pose_idx, error = pose_classifier(posepoints)  # 포즈 분류하기
+pose_img = cut_vid(frame, pose_idx)  # pose_img 는 리스트 입니다..
+draw_image(pose_img, pose_idx, posepoints)  # 골격 그리기
+image = cut_img(posepoints, pose_img, pose_idx, 0)  # 서버에 전송할 7가지 이미지 자르기(포즈 자세히 부분에 사용자에게 보여줄거)
+score, feedback_list = assess_pose(posepoints, pose_idx)  # 포즈 평가하기
